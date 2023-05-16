774abb88b97ebed5619200561ef82b6693f47b4a
DimitarRDimitrov <dimtiarrosenov.dimitrov@gmail.com>
2023-05-16 12:52:01 +0300

    springboot-initializer project backbone

---
+++ src/main/java/io/localhost/venlyassesment/VenlyAssesmentApplication.java
@@ -0,0 +1,13 @@
+package io.localhost.venlyassesment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VenlyAssesmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VenlyAssesmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/localhost/venlyassesment/VenlyAssesmentApplicationTests.java
@@ -0,0 +1,13 @@
+package io.localhost.venlyassesment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VenlyAssesmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
